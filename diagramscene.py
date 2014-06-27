fdea81524d2e33338189c0bf06f0cc2cd697094d
barsandcat <barsandcat@gmail.com>
2014-06-27 20:45:00 +0300

    Remove last bit of DiagramScene example

--- diagramscene.py
+++ diagramscene.py
@@ -1,45 +1,5 @@
 #!/usr/bin/python3
 
-#############################################################################
-##
-## Copyright (C) 2013 Riverbank Computing Limited.
-## Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
-## All rights reserved.
-##
-## This file is part of the examples of PyQt.
-##
-## $QT_BEGIN_LICENSE:BSD$
-## You may use this file under the terms of the BSD license as follows:
-##
-## "Redistribution and use in source and binary forms, with or without
-## modification, are permitted provided that the following conditions are
-## met:
-##   * Redistributions of source code must retain the above copyright
-##     notice, this list of conditions and the following disclaimer.
-##   * Redistributions in binary form must reproduce the above copyright
-##     notice, this list of conditions and the following disclaimer in
-##     the documentation and/or other materials provided with the
-##     distribution.
-##   * Neither the name of Nokia Corporation and its Subsidiary(-ies) nor
-##     the names of its contributors may be used to endorse or promote
-##     products derived from this software without specific prior written
-##     permission.
-##
-## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-## LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-## A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-## OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-## LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-## DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-## THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-## $QT_END_LICENSE$
-##
-#############################################################################
-
 from ProductionScheme import ProductionScheme
 from ProductionLine import ProductionLine
 
@@ -116,6 +76,7 @@ class ProcessGraphic(QGraphicsItem):
 			self.parents.append(aGraphic)
 
 	def paint(self, painter, option, widget=None):
+		painter.fillRect(-100, -100, 200, 200, Qt.white)
 		painter.drawRoundedRect(-100, -100, 200, 200, 10, 10)
 		painter.drawText(QPointF(0, 0), self.process.scheme.GetName())
 
@@ -173,101 +134,26 @@ def FillScene(aScene, aGraphics):
 	aScene.setSceneRect(QRectF(0, 0, sceneHeight, sceneWidth))
 	aScene.clear()
 
+	## Set process positions
 	for graphic in aGraphics:
 		x = sceneWidth - (graphic.col + 0.5) * colWidth
 		y = (graphic.row + 0.5) * rowHeigth
 		graphic.setPos(QPointF(x, y))
 		aScene.addItem(graphic)
 
-class Arrow(QGraphicsLineItem):
-	def __init__(self, startItem, endItem, parent=None, scene=None):
-		super(Arrow, self).__init__(parent)
+	## Add lines
+	queue = [aGraphics[0]]
+	done = set()
+	while queue:
+		graphic = queue.pop(0)
+		if graphic not in done:
+			done.add(graphic)
+			queue.extend(graphic.children)
+			for parent in graphic.parents:
+				line = QGraphicsLineItem(QLineF(parent.pos(), graphic.pos()))
+				line.setZValue(-1000)
+				aScene.addItem(line)
 
-		self.arrowHead = QPolygonF()
-
-		self.myStartItem = startItem
-		self.myEndItem = endItem
-		self.setFlag(QGraphicsItem.ItemIsSelectable, True)
-		self.myColor = Qt.black
-		self.setPen(QPen(self.myColor, 2, Qt.SolidLine, Qt.RoundCap,
-				Qt.RoundJoin))
-
-	def setColor(self, color):
-		self.myColor = color
-
-	def startItem(self):
-		return self.myStartItem
-
-	def endItem(self):
-		return self.myEndItem
-
-	def boundingRect(self):
-		extra = (self.pen().width() + 20) / 2.0
-		p1 = self.line().p1()
-		p2 = self.line().p2()
-		return QRectF(p1, QSizeF(p2.x() - p1.x(), p2.y() - p1.y())).normalized().adjusted(-extra, -extra, extra, extra)
-
-	def shape(self):
-		path = super(Arrow, self).shape()
-		path.addPolygon(self.arrowHead)
-		return path
-
-	def updatePosition(self):
-		line = QLineF(self.mapFromItem(self.myStartItem, 0, 0), self.mapFromItem(self.myEndItem, 0, 0))
-		self.setLine(line)
-
-	def paint(self, painter, option, widget=None):
-		if (self.myStartItem.collidesWithItem(self.myEndItem)):
-			return
-
-		myStartItem = self.myStartItem
-		myEndItem = self.myEndItem
-		myColor = self.myColor
-		myPen = self.pen()
-		myPen.setColor(self.myColor)
-		arrowSize = 20.0
-		painter.setPen(myPen)
-		painter.setBrush(self.myColor)
-
-		centerLine = QLineF(myStartItem.pos(), myEndItem.pos())
-		endPolygon = myEndItem.polygon()
-		p1 = endPolygon.first() + myEndItem.pos()
-
-		intersectPoint = QPointF()
-		for i in endPolygon:
-			p2 = i + myEndItem.pos()
-			polyLine = QLineF(p1, p2)
-			intersectType = polyLine.intersect(centerLine, intersectPoint)
-			if intersectType == QLineF.BoundedIntersection:
-				break
-			p1 = p2
-
-		self.setLine(QLineF(intersectPoint, myStartItem.pos()))
-		line = self.line()
-
-		angle = math.acos(line.dx() / line.length())
-		if line.dy() >= 0:
-			angle = (math.pi * 2.0) - angle
-
-		arrowP1 = line.p1() + QPointF(math.sin(angle + math.pi / 3.0) * arrowSize,
-										math.cos(angle + math.pi / 3) * arrowSize)
-		arrowP2 = line.p1() + QPointF(math.sin(angle + math.pi - math.pi / 3.0) * arrowSize,
-										math.cos(angle + math.pi - math.pi / 3.0) * arrowSize)
-
-		self.arrowHead.clear()
-		for point in [line.p1(), arrowP1, arrowP2]:
-			self.arrowHead.append(point)
-
-		painter.drawLine(line)
-		painter.drawPolygon(self.arrowHead)
-		if self.isSelected():
-			painter.setPen(QPen(myColor, 1, Qt.DashLine))
-			myLine = QLineF(line)
-			myLine.translate(0, 4.0)
-			painter.drawLine(myLine)
-			myLine.translate(0,-8.0)
-			painter.drawLine(myLine)
-	
 
 class MainWindow(QMainWindow):
  
