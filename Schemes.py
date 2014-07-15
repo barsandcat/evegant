a58e1df1e01f97660cc308786f31709afe49a562
barsandcat <barsandcat@gmail.com>
2014-07-15 10:42:59 +0300

    Remove LazyMarketGroup

--- Schemes.py
+++ Schemes.py
@@ -48,9 +48,6 @@ def CreateSchemesTree(connection):
 			group.AppendChild(child)
 
 
-	#treeRoot.AppendChild(LazyMarketGroup(2, "Blueprints", treeRoot, connection))
-	#treeRoot.AppendChild(LazyMarketGroup(54, "Ore", treeRoot, connection))
-	#treeRoot.AppendChild(LazyMarketGroup(493, "Ice Ore", treeRoot, connection))
 	return treeRoot
 
 
@@ -177,61 +174,3 @@ class MarketGroup:
 	def GetOutputs(self):
 		return []
 
-
-class LazyMarketGroup:
-	def __init__(self, aMarketGroupId, aName, aParent, aDBConnection):
-		self.parent = aParent
-		self.name = aName
-		self.children = None
-		self.marketGroupId = aMarketGroupId
-		self.db = aDBConnection
-
-	def CacheChildren(self):
-		if self.children == None:
-			self.children = []
-			cursor = self.db.cursor()
-			#Load child market groups
-			cursor.execute("SELECT marketGroupID, marketGroupName "
-				"FROM invMarketGroups WHERE parentGroupID = ?", 
-				(self.marketGroupId,))			
-			
-			for row in cursor:
-				marketGroupId = row[0]
-				name = row[1]
-				self.children.append(LazyMarketGroup(marketGroupId, name, self, self.db))
-
-			#Load child types, if it is blueprint - should have not null blueprintID
-			cursor.execute("SELECT typeID, blueprintTypeID "
-				"FROM invTypes LEFT JOIN invBlueprintTypes ON typeID = blueprintTypeID "
-				"WHERE marketGroupID = ?", (self.marketGroupId,))
-
-			for row in cursor:
-				if row[1]:
-					blueprint = LoadBlueprint(self.db.cursor(), row[1], self)
-					self.children.append(blueprint)
-				else:
-					refine = LoadRefine(self.db.cursor(), row[0], self)
-					self.children.append(refine)
-
-
-	def GetChild(self, row):
-		self.CacheChildren()
-		return self.children[row]
-
-	def GetChildCount(self):
-		self.CacheChildren()
-		return len(self.children)
-
-	def GetIndexOfChild(self, aChild):
-		self.CacheChildren()
-		return self.children.index(aChild)
-
-	def GetName(self):
-		return self.name
-
-	def GetParent(self):
-		return self.parent
-
-	def GetOutputs(self):
-		return []
-	
