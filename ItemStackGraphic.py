701c614fc3ad99296f972d32dcf6375e5d2f46e0
barsandcat <barsandcat@gmail.com>
2015-04-03 23:19:27 +0300

    Calculate graphics item rect based on content

--- ItemStackGraphic.py
+++ ItemStackGraphic.py
@@ -2,20 +2,22 @@
 
 from PyQt5.QtCore import QPointF, QRect, QRectF, QSize, QSizeF, Qt
 from PyQt5.QtWidgets import QGraphicsItem, QGraphicsPixmapItem
+from PyQt5.QtGui import QFontMetricsF, QFont
 
 class ItemStackGraphic(QGraphicsItem):
 	def __init__(self, aItemStack, aParent, aPos, aToolkitTypes):
 		super().__init__(aParent)
 		self.setPos(aPos)
 		self.itemStack = aItemStack
-		self.rect = QRectF(-35, 0, 70, 35)
+		self.font = QFont()
 		icon = QGraphicsPixmapItem(aToolkitTypes.GetTypePixmap(self.itemStack.itemId, 32), self)
 		icon.setPos(QPointF(-33, 2))
 
 	def paint(self, painter, option, widget=None):
-		painter.fillRect(self.rect, Qt.white)
-		painter.drawRect(self.rect)
-		painter.drawText(self.rect, Qt.AlignVCenter + Qt.AlignRight, str(self.itemStack.ammount))
+		rect = self.boundingRect()
+		painter.fillRect(rect, Qt.white)
+		painter.drawRect(rect)
+		painter.drawText(rect, Qt.AlignVCenter + Qt.AlignRight, str(self.itemStack.ammount))
 
 	def GetInScenePos(self):
 		return self.scenePos() + QPointF(-35, 17)
@@ -24,7 +26,9 @@ class ItemStackGraphic(QGraphicsItem):
 		return self.scenePos() + QPointF(35, 17)
 
 	def boundingRect(self):
-		return self.rect
+		fm = QFontMetricsF(self.font)
+		width = 32 + fm.width(str(self.itemStack.ammount))
+		return QRectF(-35, 0, width, 35)
 	
 	def GetItemId(self):
 		return self.itemStack.itemId
